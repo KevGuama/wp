8119602ee995458a17d408cba73d6a26d237af6f
KevGuama <kevguama@gmail.com>
2024-07-11 00:20:19 +0300

    pUblishSite

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-962.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-24a0b4ad', {"layout":"number","heading":"Satisfied Clients","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":150,"totalNumber":150,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-14824e59', {"layout":"number","heading":"Years of Experience","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":15,"totalNumber":150,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-604bbe6f', {"layout":"number","heading":"Completed Projects","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":25,"totalNumber":250,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-f4dacab3' );
+});
+ });
