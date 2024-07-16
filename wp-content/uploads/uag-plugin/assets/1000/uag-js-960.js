1b90113e9cd1070cd4b5939bfe1e3ad04ac13d38
KevGuama <kevguama@gmail.com>
2024-07-17 02:38:51 +0300

    Site1.0

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-960.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-960.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-f4dacab3' );
+	UAGBButtonChild.init( '.uagb-block-3f8b5008' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-66a07f3a' );
 });
  });
